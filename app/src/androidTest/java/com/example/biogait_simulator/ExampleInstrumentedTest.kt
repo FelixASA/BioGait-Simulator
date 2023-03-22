4954787cf94bc03c034749f4a26f1660587c7d98
FelixASA <felix.arredondo@uabc.edu.mx>
2023-03-22 13:44:08 -0700

    Version con Slidder y apk arreglada

--- app/src/androidTest/java/com/example/biogait_simulator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/biogait_simulator/ExampleInstrumentedTest.kt
@@ -1,7 +1,9 @@
 package com.example.biogait_simulator
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+//import android.support.test.InstrumentationRegistry
+//import android.support.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 
 import org.junit.Test
 import org.junit.runner.RunWith
